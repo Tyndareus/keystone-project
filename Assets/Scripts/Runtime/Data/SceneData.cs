071e7d975aa43d34d0b513beb9f2f22dfaa15865
Danny <dannyuk@live.co.uk>
2022-01-26 17:49:15 +0000

    Added scene data
    This is in control of the flow; main scene and selection scene are 'unique' scenes that aren't randomly accessible, any minigame/world scene should go in scene order

---
+++ Assets/Scripts/Runtime/Data/SceneData.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName =  "Keystone/Scene Data")]
+public class SceneData : ScriptableObject
+{
+    public SceneObject mainScene;
+    public SceneObject selectionScene;
+    public List<SceneObject> sceneOrder;
+    public static int currentScene;
+}
+
+[Serializable]
+public class SceneObject
+{
+    [SceneSelector] public int scene;
+}
