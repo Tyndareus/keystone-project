41f7485e64852bc5f58c9f1357bf7acdf12aa807
Danny <dannyuk@live.co.uk>
2022-02-04 19:52:18 +0000

    CardManager updated
    Now uses fade in to start and fades out to end
    
    All the cards are instantiated and can be dynamically set, though the scaling is not dynamically handled.

--- Assets/Scripts/Runtime/MemoryGame/CardManager.cs
+++ Assets/Scripts/Runtime/MemoryGame/CardManager.cs
@@ -2,31 +2,105 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardManager : MonoBehaviour
 {
     [SerializeField, Range(0f, 2.0f)] private float cardDisplaySpeed;
     [SerializeField, Range(1f, 10f)] private float cardMemoryTime;
-
+    [SerializeField, Range(4, 13)] private int difficulty;
+    
     [SerializeField] private Transform containerTransform;
     [SerializeField] private List<CardData> cardSelection;
+    [SerializeField] private GameObject cardPrefab;
+    [SerializeField] private GridLayoutGroup layout;
 
-    private List<Card> playerSelection;
+    [SerializeField] private FadeManager fadeManager;
+
+    private List<CardData> selectedCards;
+    private Card[] playerSelection;
     
     private bool canSelect;
-    
+
     private void Start()
     {
-        playerSelection = new List<Card>(PlayerDataManager.Instance.playerCount);
-        RandomiseCards();
+        layout.constraintCount = difficulty;
+        playerSelection = new Card[PlayerDataManager.Instance.playerCount];
 
-        StartCoroutine(PaintAndDisplay());
+        PopulateCards();
+        RandomiseCards();
+        ConstructCards();
+        PaintCards();
+    }
+
+    public void OnFadeIn() => StartCoroutine(PaintAndDisplay());
+
+    private void PopulateCards()
+    {
+        selectedCards = new List<CardData>();
+        for (int i = 0; i < difficulty; i++)
+        {
+            CardData rand = cardSelection[Random.Range(0, cardSelection.Count)];
+            if (!selectedCards.Contains(rand))
+            {
+                selectedCards.Add(rand);
+            }
+            else
+            {
+                //Unsafe but fuck it
+                i--;
+            }
+        }
     }
 
     private void RandomiseCards()
     {
         System.Random rand = new System.Random();
-        cardSelection = cardSelection.OrderBy(c => rand.Next()).ToList();
+
+        List<CardData> tempList = selectedCards.ToList();
+
+        tempList = tempList.OrderBy(c => rand.Next()).ToList();
+
+        bool needsToRandomAgain = false;
+        for (int i = 0; i < tempList.Count; i++)
+        {
+            if (tempList[i].sprite == selectedCards[i].sprite)
+            {
+                needsToRandomAgain = true;
+            }
+        }
+
+        if (needsToRandomAgain)
+        {
+            RandomiseCards();
+        }
+        else
+        {
+            selectedCards = tempList.ToList();
+        }
+    }
+
+    private void ConstructCards()
+    {
+        for (int i = 0; i < difficulty * 2; i++)
+        {
+            Instantiate(cardPrefab, containerTransform);
+        }
+    }
+
+    private void PaintCards()
+    {
+        for (int i = 0; i < containerTransform.childCount; i++)
+        {
+            Transform child = containerTransform.GetChild(i);
+            Card card = child.GetComponent<Card>();
+            card.UpdateCard(selectedCards[i % selectedCards.Count], this, cardDisplaySpeed);
+
+            if (i == selectedCards.Count - 1 && i != containerTransform.childCount - 1)
+            {
+                RandomiseCards();
+            }
+        }
     }
 
     private IEnumerator PaintAndDisplay()
@@ -38,16 +112,9 @@ public class CardManager : MonoBehaviour
         {
             Transform child = containerTransform.GetChild(i);
             Card card = child.GetComponent<Card>();
-            card.UpdateCard(cardSelection[i % cardSelection.Count], this, cardDisplaySpeed);
-            
             card.Display();
 
             for (float t = 0.0f; t <= cardDisplaySpeed; t += Time.deltaTime) yield return null;
-
-            if (i == cardSelection.Count - 1 && i != containerTransform.childCount - 1)
-            {
-                RandomiseCards();
-            }
         }
 
         for (float t = 0.0f; t <= cardMemoryTime; t += Time.deltaTime) yield return null;
@@ -61,6 +128,11 @@ public class CardManager : MonoBehaviour
         }
 
         canSelect = true;
+        
+        foreach (var uiPlayer in FindObjectsOfType<UIPlayerController>())
+        {
+            uiPlayer.EnableInput();
+        }
     }
 
     public void Select(int playerIndex, Card selectionCard)
@@ -105,11 +177,25 @@ public class CardManager : MonoBehaviour
         }
         else
         {
+            playerSelection[index].Toggle(true);
             playerSelection[index].Hide();
+            other.Toggle(true);
             other.Hide();
         }
 
         playerSelection[index] = null;
         canSelect = true;
+
+        CheckIfComplete();
     }
+
+    private void CheckIfComplete()
+    {
+        if (GetComponentsInChildren<Card>().All(x => x.HasBeenCompleted()))
+        {
+            fadeManager.FadeOut();
+        }
+    }
+
+    public void OnFadeOut() => LevelManager.Instance.LoadNextScene();
 }
