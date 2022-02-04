7ca3c564d56cc766001a973b68d58ac081cfcbc8
Danny <dannyuk@live.co.uk>
2022-02-04 19:53:36 +0000

    PlayerSelection updated
    PlayerSelection now does something, this handles a method raised from the UIPlayerController and then tells the manager that this object has been chosen.

--- Assets/Scripts/Runtime/PlayerSelection.cs
+++ Assets/Scripts/Runtime/PlayerSelection.cs
@@ -1,5 +1,13 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class PlayerSelection : Selectable
 {
+    [SerializeField] private PlayerSelectionManager manager;
+    
+    public void PlayerSelect(int playerIndex, Vector3 scale)
+    {
+        manager.SelectCharacter(playerIndex, transform.GetSiblingIndex(), scale);
+    }
 }
