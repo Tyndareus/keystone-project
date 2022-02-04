5532039d21ef2eec6973eafe52a9d56f498008a6
Danny <dannyuk@live.co.uk>
2022-02-04 19:53:01 +0000

    PlayerDataManager updated
    Removed the loading call when character has been selected, this will probably need to return at some point but instead of simply loading the next scene it should be handled by the fade out

--- Assets/Scripts/Runtime/PlayerDataManager.cs
+++ Assets/Scripts/Runtime/PlayerDataManager.cs
@@ -1,12 +1,17 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerDataManager : Singleton<PlayerDataManager>
 {
     [Range(1, 4)] public int maxPlayerCount;
     public int playerCount;
     [SerializeField] private PlayerData playerDataTemplate;
-    
+
+    [SerializeField] private List<GameObject> characters;
+    [SerializeField] private List<GameObject> idleCharacters;
+
     private PlayerData[] players;
 
     public override void Awake()
@@ -24,19 +29,24 @@ public class PlayerDataManager : Singleton<PlayerDataManager>
         playerCount++;
     }
 
-    public void OnCharacterSelected(int index, Sprite sprite)
+    public void OnCharacterSelected(int index, int charIndex, Vector3 assumedScale)
     {
-        players[index].characterSprite = sprite;
-        
-        /*if (players.Count(p => p.characterSprite != null) >=
+        players[index].character = characters[charIndex];
+        players[index].idleCharacter = idleCharacters[charIndex];
+        players[index].characterScale = assumedScale / 100f;
+
+        /*if (players.Count(p => p.character != null) >=
             maxPlayerCount)
-        {*/
+        {
             LevelManager.Instance.LoadNextScene();
-        //}
+        }*/
     }
 
     public PlayerData SelectPlayer(int index) => players[index];
-    public Sprite GetPlayerSprite(int index) => players[index].characterSprite;
+
+    public GameObject GetPlayerCharacter(int index, bool useIdle = false) =>
+        useIdle ? players[index].idleCharacter : players[index].character;
+
     public int GetPlayerScore(int index) => players[index].points;
     public void UpdatePlayerScore(int index, int score) => players[index].points = score;
 }
