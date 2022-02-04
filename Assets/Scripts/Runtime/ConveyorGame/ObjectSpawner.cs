af2d2865f9e4346d59ced083efeadf8a96fb188e
Danny <dannyuk@live.co.uk>
2022-02-04 19:46:26 +0000

    Object spawner updated
    Changed spawnable items to a sprite and now selects randomly from that list

--- Assets/Scripts/Runtime/ConveyorGame/ObjectSpawner.cs
+++ Assets/Scripts/Runtime/ConveyorGame/ObjectSpawner.cs
@@ -5,35 +5,19 @@ using UnityEngine;
 public class ObjectSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject spawnablePrefab;
-    [SerializeField] private List<ConveyorItemData> spawnableItems;
-
+    [SerializeField] private List<Sprite> spawnableItems;
     [SerializeField] private float timeBetweenSpawns;
     
-
-    public void BeginSpawning()
-    {
-        StartCoroutine(SpawnItems());
-    }
+    public void BeginSpawning() => StartCoroutine(SpawnItems());
 
     private IEnumerator SpawnItems()
     {
         while (TimerManager.InPlay)
         {
             GameObject item = Instantiate(spawnablePrefab, transform.position, Quaternion.identity);
-
-            item.GetComponent<SpriteRenderer>().sprite = spawnableItems[0].sprite;
-            
-            switch (spawnableItems[0].collision)
-            {
-                case CollisionType.Circle:
-                    item.AddComponent<BoxCollider2D>(); 
-                    break;
-                case CollisionType.Square:
-                    item.AddComponent<CircleCollider2D>();
-                    break;
-            }
-            
-            item.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1.5f, 3.75f), ForceMode2D.Impulse);
+            item.GetComponent<SpriteRenderer>().sprite = spawnableItems[Random.Range(0, spawnableItems.Count)];;
+            item.AddComponent<BoxCollider2D>();
+            item.GetComponent<Rigidbody2D>().AddForce(new Vector2(-3.5f, 4.75f), ForceMode2D.Impulse);
             
             yield return new WaitForSeconds(timeBetweenSpawns);
         }
